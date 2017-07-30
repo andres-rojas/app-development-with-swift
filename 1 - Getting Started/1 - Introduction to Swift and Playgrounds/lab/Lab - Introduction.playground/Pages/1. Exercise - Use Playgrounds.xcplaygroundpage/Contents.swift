1f26e8a5836504bb4085c888b36285022d718646
Andres Rojas <andres@conpat.io>
2017-07-30 04:51:24 -0700

    Lab - Introduction

---
+++ 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*: 
+ ## Exercise - Use Playgrounds
+ 
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+let song = "Sing About Me, I'm Dying of Thirst"
+let artist = "Kendrick Lamar"
+
+print(song, "-", artist)
+
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+let refrain = "Dying of thirst"
+
+print("Tired of running")
+print("Tired of hunting")
+print("My own kind")
+print("But retiring nothing")
+print("Tires are already screeching")
+print("The driver is rubbing")
+print("Hands on the wheel")
+print("Who said we wasn't")
+print(refrain)
+print(refrain)
+
+
+/*:
+ 
+ _Copyright © 2017 Apple Inc._
+ 
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+ 
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+ 
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ */
